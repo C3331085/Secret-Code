db923866bf6f3e63a438fc69d69ede4b89dafe1b
Mitchell Scales <47996615+C3331085@users.noreply.github.com>
2019-04-12 18:04:07 +1000

    Added file integration

--- Secret Code/main.c
+++ Secret Code/main.c
@@ -9,31 +9,45 @@
 #include <stdio.h>
 #include <string.h>
 
+int key, i, selection; //key is the rotational key, i is increment counter, selection used for menu
+char x; //not used yet
+char str[1024]; //Message to encrypt
+char str2[1024] = "LIPPS"; //message to decrypt
+void en(char* str); //Encryption function
+void de(char* str2); //Decryption function
+int menu(void); //function for menu
+int keys(void); //function for entering key for rotation
+FILE *keyss;
+FILE *Encryptions;
+FILE *Decryptions;
 
-int key, i, ii, selection;
-char x;
-char str[1024] = "AZ";
-char str2[1024] = "AZ";
-void en(char* str);
-void de(char* str2);
-int menu(void);
+
 
 
 
 
 int main() {
     
-    printf("Please Enter a key value:");
-    scanf("%d", &key);
+  
+    keys();
     menu();
     switch(selection){
-        case 2: de(str2);break;
-        case 1: en(str); break;
+        case 2: de(str2); break;
+        case 1: en(str);  break;
         case 3: ; break;
         case 4: ; break;
         case 5: ; break;
    
     }}
+int keys(void){
+    keyss = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/File.txt", "r");
+    if(keyss == NULL) {
+        perror("fopen()");
+        return 0;}
+    fscanf(keyss, "%d", &key);
+    return key;
+}
+
 int menu(void){
     printf("Please select one of the following options\n");
     printf("1: Encryption with a rotation cipher\n");
@@ -48,14 +62,22 @@ int menu(void){
 }
 
 void en(char* str){
+    Encryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Encryption.txt", "r");
+    if(Encryptions == NULL) {
+        perror("fopen()");
+        return;}
+    
+    fscanf(Encryptions, "%s", str);
+    
     for(i=0; str[i] != '\0'; i++){
         str[i] = str[i] + key;
         if(str[i] >= 'Z'){
             str[i] = str[i] - 26;
             break;}
+    }
             printf("%s\n", str);
             
-        }
+    
 }
 
 void de(char* str2){
