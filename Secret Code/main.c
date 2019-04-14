bb8942b4de0b35b8e8882543d5e0d9855ca43b50
Mitchell Scales <47996615+C3331085@users.noreply.github.com>
2019-04-14 15:54:25 +1000

    Added substitution cyphers

--- Secret Code/main.c
+++ Secret Code/main.c
@@ -10,35 +10,53 @@
 #include <string.h>
 
 int key, i, selection; //key is the rotational key, i is increment counter, selection used for menu
-char x; //not used yet
+int x; //not used yet
+
+int Hi;
+int y;
 char str[1024]; //Message to encrypt
-char str2[1024] = "LIPPS"; //message to decrypt
+char str2[1024] ; //message to decrypt
+char str3[1024] ;
+char str4[1024] ;
+char str5[1024] ;
+char str6[1024];
+char str7[1024];
 void en(char* str); //Encryption function
 void de(char* str2); //Decryption function
+void deSub(char* str3);
+void enSub(char* str5);
 int menu(void); //function for menu
 int keys(void); //function for entering key for rotation
+
 FILE *keyss;
 FILE *Encryptions;
 FILE *Decryptions;
-
+char* alphabet[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"};
+char* sub_alphabet[26] = {"Q", "W", "E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"};
 
 
 
 
 
 int main() {
+    for(i = 0; str7[i] != '\0'; i++){
+        
+    }
+        
     
-  
     keys();
     menu();
     switch(selection){
         case 2: de(str2); break;
         case 1: en(str);  break;
-        case 3: ; break;
-        case 4: ; break;
+        case 3: enSub(str3); break;
+        case 4: deSub(str5); break;
         case 5: ; break;
-   
-    }}
+     
+     }
+    }
+
+
 int keys(void){
     keyss = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/File.txt", "r");
     if(keyss == NULL) {
@@ -65,7 +83,8 @@ void en(char* str){
     Encryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Encryption.txt", "r");
     if(Encryptions == NULL) {
         perror("fopen()");
-        return;}
+        return;
+        }
     
     fscanf(Encryptions, "%s", str);
     
@@ -81,6 +100,11 @@ void en(char* str){
 }
 
 void de(char* str2){
+    Decryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Decryption.txt", "r");
+    if(Decryptions == NULL) {
+        perror("fopen()");
+        return;}
+    fscanf(Decryptions, "%s", str2);
     for(i=0; str2[i] != '\0'; i++){
         str2[i] = str2[i] - key;
         
@@ -91,8 +115,43 @@ void de(char* str2){
     printf("%s\n", str2);
 }
 
+void enSub(char* str3){
+    Encryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Encryption.txt", "r");
+    if(Encryptions == NULL) {
+        perror("fopen()");
+        return;}
+    fscanf(Encryptions, "%s", str3);
+    for(i=0; str3[i] != '\0';i++){
+        x = (int)str3[i]- 65;
+        str4[i] = *sub_alphabet[x];}
+    
+    printf("%s\n", str4);
+    
+}
+    
+void deSub(char* str5){
+    Decryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Decryption.txt", "r");
+    if(Decryptions == NULL) {
+        perror("fopen()");
+        return;}
+    fscanf(Decryptions, "%s", str5);
+    for(y = 0 ; str5[y] != '\0'; y++){
+        for ( i = 0; i < 26; i++)
+        {
+            if (str5[y] == *sub_alphabet[i])
+            {
+                Hi = i;
+                str6[y] = *alphabet[Hi];}
+        }
+    }
+    
+    printf("%s\n", str6);
+    
+    }
+    
 
     
+
 
     
 
