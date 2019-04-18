6784b6982915a136875fe0f9f6c6cfc617181e5d
Mitchell Scales <47996615+C3331085@users.noreply.github.com>
2019-04-18 11:25:30 +1000

    Added file output and description for all functions

--- Secret Code/main.c
+++ Secret Code/main.c
@@ -6,56 +6,60 @@
 //  Copyright © 2019 Mitchell Scales. All rights reserved.
 //
 
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> // to use printf and scanf functions
+#include <string.h> //to manipulate strings
 
-int key, i, selection; //key is the rotational key, i is increment counter, selection used for menu
+int key, i, selection; //key is the rotational key, i is increment counter for loops, selection used for menu selection
 int x; //not used yet
 
-int Hi;
-int y;
+int Alpha_index;// Index counter for the alphabet
+int y;// another increment counter used when i is already being used
 char str[1024]; //Message to encrypt
 char str2[1024] ; //message to decrypt
-char str3[1024] ;
-char str4[1024] ;
-char str5[1024] ;
-char str6[1024];
-char str7[1024];
-void en(char* str); //Encryption function
-void de(char* str2); //Decryption function
-void deSub(char* str3);
-void enSub(char* str5);
+char str3[1024] ; // message to encrypt through substitution
+char str4[1024] ; // Encrypted text saved as str4
+char str5[1024] ; //decryption message for substitution
+char str6[1024]; //decrypted message from substition
+char str7[1024]; // not used yet
+void en(void); //Encryption function using rotation with no input or output requried
+void de(void); //Decryption function  using rotation with no input of output required
+void deSub(void); // Decryption function  using substitution with no input of output required
+void enSub(void); // Encryption function  using substitution with no input of output required
 int menu(void); //function for menu
 int keys(void); //function for entering key for rotation
 
-FILE *keyss;
-FILE *Encryptions;
-FILE *Decryptions;
-char* alphabet[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"};
-char* sub_alphabet[26] = {"Q", "W", "E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"};
+FILE *keyss; //Creating a file name called keyss to call later to store the key value
+FILE *Outputs; // Creating a file named outputs to output the text to
+FILE *Encryptions; //createing a file name called Encryptions to call later to hold words to encrypt
+FILE *Decryptions; // creating a file name called decryptions to call later to hold words to decrypt
+char* alphabet[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"}; //creating an array of characters to store the alphabet
+char* sub_alphabet[26] = {"Q", "W", "E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"};// creating an array for a substitution alphabet
 
 
 
 
 
 int main() {
-    for(i = 0; str7[i] != '\0'; i++){
-        
-    }
-        
     
-    keys();
-    menu();
-    switch(selection){
-        case 2: de(str2); break;
-        case 1: en(str);  break;
-        case 3: enSub(str3); break;
-        case 4: deSub(str5); break;
+ //main structure is just to first call keys funtion to intialise the integer keys to a user determined value, then runs the menu for the user to select which function they need, with all the functions running in a switch case and all functions in their own function.
+    
+    keys();// calling the keys funtion to run
+    menu();// calling the menu function to run
+    switch(selection){ // adding a switch as a menu selector from the integer called selection that the user types in.
+        case 2: de(); break;
+        case 1: en();  break;
+        case 3: enSub(); break;
+        case 4: deSub(); break;
         case 5: ; break;
      
      }
     }
-
+/*
+ Inputs: No inputs needed, just to be called.
+ Outputs: integer called key
+ Function: Opens a file called keyss where a number is stored for the rotational cypher, reads that number and stores it in an int called key.
+ Limitations:Expects an integer
+ */
 
 int keys(void){
     keyss = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/File.txt", "r");
@@ -65,7 +69,12 @@ int keys(void){
     fscanf(keyss, "%d", &key);
     return key;
 }
-
+/*
+ Inputs: There are no inputs needed for the function, it just needs to be called
+ Outputs: Returns the integer selection; that is the menu item number from scanf
+ Function: Prints a menu to the terminal to select the function task, with a user to then type the menu number option which is stored in selection.
+ Limitations: Expecting an int between 1-6.
+ */
 int menu(void){
     printf("Please select one of the following options\n");
     printf("1: Encryption with a rotation cipher\n");
@@ -78,8 +87,13 @@ int menu(void){
     scanf("%d", &selection);
     return selection;
 }
-
-void en(char* str){
+/*
+ Inputs: No input just needs to be called
+ Outputs: Outputs to terminal the string of the encrypted text
+ Function: Calls a string from the file encryptions and saves it as str, then adds the value of key to it (ASCII code ) which then prints the final string of characters. Has a if statment to make sure if the letter is higher than z it minus 26 to get back to the start of the alphabet.
+ Limitations: Must have only capital letters in the encryption file and terminates at the first space/ gap key.
+ */
+void en(){
     Encryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Encryption.txt", "r");
     if(Encryptions == NULL) {
         perror("fopen()");
@@ -94,12 +108,23 @@ void en(char* str){
             str[i] = str[i] - 26;
             break;}
     }
-            printf("%s\n", str);
+        printf("%s\n", str);
+    Outputs = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Output.txt", "a");
+    if(Outputs == NULL) {
+        perror("fopen()");
+        return;}
+   fprintf(Outputs, "%s\n", str);
+    }
             
     
-}
 
-void de(char* str2){
+/*
+ Inputs: No input just needs to be called
+ Outputs: Outputs to terminal the string of the decrypted text as str2
+ Function: Calls a string from the file decryptions and saves it as str2, then takes away the value of key to it (ASCII code ) which then prints the final string of characters. Has a if statment to make sure if the letter is lower than 'a' it adds 26 to get back to the end of the alphabet.
+ Limitations: Must have only capital letters in the decryption file and terminates at the first space/ gap key.
+ */
+void de(){
     Decryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Decryption.txt", "r");
     if(Decryptions == NULL) {
         perror("fopen()");
@@ -114,8 +139,13 @@ void de(char* str2){
     };
     printf("%s\n", str2);
 }
-
-void enSub(char* str3){
+/*
+ Inputs: No input just needs to be called
+ Outputs: to terminal the string of the encrypted text as str 4
+ Function: Calls a file named encryption and saves it under str3, the value of each charater in the string is translated to a posistion in the array sub_alphabet which is then stored in str4 and printed to the terminal. Repeats for each letter to form a substitution cypher
+ Limitations:Must have only capital letters in the decryption file and terminates at the first space/ gap key.
+ */
+void enSub(){
     Encryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Encryption.txt", "r");
     if(Encryptions == NULL) {
         perror("fopen()");
@@ -128,8 +158,13 @@ void enSub(char* str3){
     printf("%s\n", str4);
     
 }
-    
-void deSub(char* str5){
+/*
+ Inputs: No input just needs to be called
+ Outputs: to terminal the string of the decrypted text as str 6
+ Function: Calls a file named decryption and saves it under str5, the value of each charater in the string is translated to a posistion in the array alphabet which is then stored in str5 and printed to the terminal. Repeats for each letter to form a substitution cypher
+ Limitations:Must have only capital letters in the decryption file and terminates at the first space/ gap key.
+ */
+void deSub(){
     Decryptions = fopen("/Users/mitchellscales/Desktop/UNI/ENGG1003/Assesments/Secret Code/Secret Code/Decryption.txt", "r");
     if(Decryptions == NULL) {
         perror("fopen()");
@@ -140,8 +175,8 @@ void deSub(char* str5){
         {
             if (str5[y] == *sub_alphabet[i])
             {
-                Hi = i;
-                str6[y] = *alphabet[Hi];}
+                Alpha_index = i;
+                str6[y] = *alphabet[Alpha_index];}
         }
     }
     
